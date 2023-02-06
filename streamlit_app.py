03b2f319466e2fb30688e334d41569402b725bd8
Kshrisha <46555243+Kshrisha@users.noreply.github.com>
2023-02-06 16:11:12 +0530

    Update streamlit_app.py
    Added Emoji

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,7 @@ import streamlit
 streamlit.title('New movie play list')
 
 streamlit.header(':calendar: Hollywood')
-streamlit.text('Secret life of milley walter')
-streamlit.text('Silent Voice')
-streamlit.text('The space between us')
-streamlit.text('Upside down')
+streamlit.text(':movie_camera: Secret life of milley walter')
+streamlit.text(':film_projector: Silent Voice')
+streamlit.text(':film_strip: The space between us')
+streamlit.text(':camera_flash: Upside down')
