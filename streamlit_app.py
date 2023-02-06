fdb330ff2567d9209d19cd8decd53033626ed181
Kshrisha <46555243+Kshrisha@users.noreply.github.com>
2023-02-06 16:12:00 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,7 @@ import streamlit
 
 streamlit.title('New movie play list')
 
-streamlit.header(':calendar: Hollywood')
+streamlit.header(':calendar: Hollywood :movie_camera:')
 streamlit.text(':movie_camera: Secret life of milley walter')
 streamlit.text(':film_projector: Silent Voice')
 streamlit.text(':film_strip: The space between us')
