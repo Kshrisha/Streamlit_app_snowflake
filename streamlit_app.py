76f025089bb099f8f867f097b89ab36956dcaa33
Kshrisha <46555243+Kshrisha@users.noreply.github.com>
2023-02-06 23:22:19 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,5 +10,12 @@ streamlit.text('🎬 Silent Voice')
 streamlit.text('📹 The space between us')
 streamlit.text('📽️ Upside down')
 
+# Display the table on the page.
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+my_fruit_list = my_fruit_list.set_index('Fruit')
+
+streamlit.header('Healthy fruits')
+# Let's put a pick list here so they can pick the fruit they want to include 
+streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
+
 streamlit.dataframe(my_fruit_list)
