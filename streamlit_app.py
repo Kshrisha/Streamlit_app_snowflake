20f3427306be2bc387b33c0976a659c5418325f1
Kshrisha <46555243+Kshrisha@users.noreply.github.com>
2023-02-06 16:04:31 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,10 @@
 #create main python file
 import streamlit
 
-streamlit.title('My parents new healty Diner')
+streamlit.title('My new movie play list')
 
+streamlit.header('Hollywood')
+streamlit.text('Secret life of milley walter')
+streamlit.text('Silent Voice')
+streamlit.text('The space between us')
+streamlit.text('Upside down')
